f146a305189fffbf0534eead47652850203a2313
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2022-02-13 17:01:42 +0100

    initialise dev branch

--- src/services/vehicles.js
+++ src/services/vehicles.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const getVehicles = async () => {
   const vehicules = await axios.get(
-    'https://random-data-api.com/api/vehicle/random_vehicle?size=10',
+    'https://random-data-api.com/api/vehicle/random_vehicle?size=50',
   )
   return vehicules.status === 200 ? vehicules.data : []
 }
