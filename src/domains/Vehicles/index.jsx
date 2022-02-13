85a13f20956767b81cac1e2e0c0e9bbfc05ef688
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2022-02-13 16:32:02 +0100

    Fix and review domains Vehicle index

--- src/domains/Vehicles/index.jsx
+++ src/domains/Vehicles/index.jsx
@@ -1,58 +1,50 @@
-import React, { useEffect, useState } from "react";
-import VehicleCard from "../../components/VehicleCard/VehicleCard";
-import { getVehicles } from "../../services/vehicles";
-import styles from "./VehiclesIndex.module.scss";
+import React, { useEffect, useState } from 'react'
+import VehicleCard from '../../components/VehicleCard/VehicleCard'
+import { getVehicles } from '../../services/vehicles'
+import styles from './VehiclesIndex.module.scss'
 
 const VehiculeIndex = () => {
-  const [cars, setCars] = useState(null);
-  let order = false;
-
+  // Fix : initialisation de la state cars par un empty Array
+  const [cars, setCars] = useState([])
+  // Fix : suppréssion de la variable order et création d'une state order.
+  const [order, setOrder] = useState(true)
   useEffect(() => {
     const fetchCars = async () => {
-      setCars(await getVehicles());
-    };
-    fetchCars();
-  }, []);
+      setCars(await getVehicles())
+    }
+    fetchCars()
+  }, [])
 
-  if (!cars) {
-    return <>loading</>;
-  }
 
-  if (order) {
-    return (
-      <>
-        <h3>My garage</h3>
-        <div className={styles.actionsContainer}>
-          <button onClick={() => (order = false)}>Reset order</button>
-        </div>
-        <div className={styles.cardsContainer}>
-          {cars.map((car) => {
-            if (car.color !== "Black" && car.color !== "White") return <> </>;
-            return <VehicleCard key={car.id} vehicle={car} />;
-          })}
-        </div>
-      </>
-    );
-  }
-
-  return (
+  return cars.length === 0 ? (
+    <>loading</>
+  ) : (
     <>
       <h3>My garage</h3>
       <div className={styles.actionsContainer}>
         <button
-          style={{ border: "red 1px solid", cursor: "pointer" }}
-          onClick={() => (order = true)}
+          // Review : passage en classe active
+          className={
+            order ? styles.switchButtonEnable : styles.switchButtonDisable
+          }
+          onClick={() => setOrder(!order)}
         >
-          Only black & white
+          {order ? `Only black & white` : 'Reset oder'}
         </button>
       </div>
       <div className={styles.cardsContainer}>
-        {cars.map((car) => (
-          <VehicleCard key={car.id} vehicle={car} />
-        ))}
+        
+        {/* Fix : utilisation des filter */}
+        {cars.map((car ,i) =>
+          order ? (
+            <VehicleCard key={i} vehicle={car}></VehicleCard>
+          ) : car.color === 'Black' || car.color === 'White' ? (
+            <VehicleCard key={i} vehicle={car}></VehicleCard>
+          ) : null,
+        )}
       </div>
     </>
-  );
-};
+  )
+}
 
-export default VehiculeIndex;
+export default VehiculeIndex
