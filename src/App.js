ef58a19802ae252dc43ea51f03999dca33edeb7c
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2022-02-13 17:32:25 +0100

    create vehicule page and add route inside app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import VehiculeIndex from "./domains/Vehicles";
+import Vehicule from "./domains/Vehicule/Vehicule";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<VehiculeIndex />}></Route>
+      <Route path="/vehicule/:id" element={<Vehicule />}></Route>
       </Routes>
     </BrowserRouter>
-   
   );
 }
 
---
+++ src/domains/Vehicule/Vehicule.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Vehicule() {
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    )
+}
