59cbd0e663fb6b37c358f821a2afc03cd4706412
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2022-02-13 16:52:21 +0100

    add empty array if result catch from API is different to status 200

--- src/services/vehicles.js
+++ src/services/vehicles.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const getVehicles = async() => {
-    const vehicules = await axios.get(
-      'https://random-data-api.com/api/vehicle/random_vehicle?size=10',
-    )
-    return vehicules.data
+const getVehicles = async () => {
+  const vehicules = await axios.get(
+    'https://random-data-api.com/api/vehicle/random_vehicle?size=10',
+  )
+  return vehicules.status === 200 ? vehicules.data : []
 }
 
 export default getVehicles
